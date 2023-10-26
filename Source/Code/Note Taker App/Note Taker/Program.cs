445f9a086142b4996f492e50e1d728a806d1c82c
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-10-26 16:03:49 +0000

    Start including logic

--- Source/Code/Note Taker App/Note Taker/Program.cs
+++ Source/Code/Note Taker App/Note Taker/Program.cs
@@ -5,16 +5,29 @@ namespace Note_Taker
 {
     internal class Program
     {
+        private const string DontHaveNotes = $"You don't have notes!";
+        private const string EnterCommand = "Enter command! Only number!";
+        private const string EnterChoices = "1- add; 2- open; 3- delete";
+        
         static void Main(string[] args)
         {
             using NoteTakerContext context = new NoteTakerContext();
 
-            var list = context.Processes.ToList();
-            foreach (var po in list)
+            var notes = context.Notes.Include(n=>n.ProcessNavigation).ToList();
+            if (notes.Count==0)
             {
-                Console.WriteLine($"{po.Name}");
-                
+                Console.WriteLine(DontHaveNotes);
             }
+            else
+            {
+                foreach (var no in notes)
+                {
+                    Console.WriteLine($"{no.Id}{no.Title}");
+                }
+            }
+            Console.WriteLine(EnterCommand);
+            Console.WriteLine(EnterChoices);
+            Console.ReadLine();
         }
     }
 }
