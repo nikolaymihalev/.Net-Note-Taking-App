1270fdaa3ab9368e918311e0b54e9df9a2a40451
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-10-26 10:30:08 +0000

    Setting project

---
+++ Source/Code/Note Taker App/Note Taker/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Note_Taker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
