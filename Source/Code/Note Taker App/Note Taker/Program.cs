20fee2edb510a7735bef23c51fb22c79454909c9
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-10-26 13:12:31 +0000

    Update Program.cs

--- Source/Code/Note Taker App/Note Taker/Program.cs
+++ Source/Code/Note Taker App/Note Taker/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace Note_Taker
+﻿using Microsoft.EntityFrameworkCore;
+using Note_Taker.DbModels;
+
+namespace Note_Taker
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            using NoteTakerContext context = new NoteTakerContext();
+
+            var list = context.Processes.ToList();
+            foreach (var po in list)
+            {
+                Console.WriteLine($"{po.Name}");
+                
+            }
         }
     }
 }
